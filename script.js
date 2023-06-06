2ec1dac451276536555b08107b4646f7efef7775
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-06 16:06:47 +0200

    structured todoList array

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ createApp({
     data(){
         return{
             todoList: [
-                'mangiare',
-                'dormire',
-                'andare al supermercato',
-                'visitare la luna',
-                'uscire il cane',
-                'scrivo ai tuti che esco',
-                'scendi la valigia',
-                'distruggere il capitalismo',
+            {}, 
+            {}, 
+            {}, 
+            {}, 
+            {}, 
+            {}, 
+            {}, 
+            {}, 
             ],
             newElement:''
         }
